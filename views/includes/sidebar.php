d23f9c3e2c09f1aba95b533e3bd664d7d69fd2c8
isturiz <isturizmauricio@gmail.com>
2023-11-28 04:40:55 -0400

    hidden sales

--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -1,7 +1,7 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-gray-200 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-gray-200 dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
-         <li>
+         <li class="hidden">
             <a href="#" class="flex items-center p-2 text-gray-900 rounded-2xl dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
